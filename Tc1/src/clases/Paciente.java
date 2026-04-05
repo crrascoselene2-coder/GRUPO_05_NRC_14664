c9ff1c7c8210fbfb9498533da073fe10be936114
Gabriel <gevs0783@gmail.com>
2026-04-04 19:21:59 -0500

    segundo avance

--- Tc1/src/clases/Paciente.java
+++ Tc1/src/clases/Paciente.java
@@ -7,5 +7,12 @@ private String dni;
 private String nom;
 private String cita;
 private Date hora;
+public Paciente(String dni, String nom, String cita, Date hora) {
+	super();
+	this.dni = dni;
+	this.nom = nom;
+	this.cita = cita;
+	this.hora = hora;
+}
 
 }
