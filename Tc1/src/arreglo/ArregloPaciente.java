b47a6e046fe15eddecee0139d11b580241b535f8
Gabriel <gevs0783@gmail.com>
2026-04-05 17:58:37 -0500

    A

--- Tc1/src/arreglo/ArregloPaciente.java
+++ Tc1/src/arreglo/ArregloPaciente.java
@@ -1,12 +1,13 @@
 package arreglo;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import clases.*;
 public class ArregloPaciente {
 private ArrayList<Paciente>paci;
 public ArregloPaciente() {
 	paci=new ArrayList<Paciente>();
-	
+    adicionar(null);
 }
 public void adicionar(Paciente x) {
 	paci.add(x);
