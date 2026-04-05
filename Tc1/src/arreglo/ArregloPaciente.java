bb97120eabd71102baac288038844ddda3c6af06
Gabriel <gevs0783@gmail.com>
2026-04-05 09:59:05 -0500

    ..

---
+++ Tc1/src/arreglo/ArregloPaciente.java
@@ -0,0 +1,11 @@
+package arreglo;
+
+import java.util.ArrayList;
+import clases.*;
+public class ArregloPaciente {
+private ArrayList<Paciente>paci;
+public ArregloPaciente() {
+	paci=new ArrayList<Paciente>();
+	
+}
+}
