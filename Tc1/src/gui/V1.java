a316cbbb5b74ad9a60973bb947c56c3cd0cc6ef4
Gabriel <gevs0783@gmail.com>
2026-04-04 21:33:19 -0500

    label Título Gabriel

--- Tc1/src/gui/V1.java
+++ Tc1/src/gui/V1.java
@@ -5,21 +5,14 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-<<<<<<< HEAD
 import javax.swing.JLabel;
-=======
-import javax.swing.JButton;
->>>>>>> 2893f46f2cc41a0b90c8b78a38597ce94268001a
+import java.awt.Font;
 
 public class V1 extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-<<<<<<< HEAD
 	private JLabel lblNewLabel;
-=======
-	private JButton btnNewButton;
->>>>>>> 2893f46f2cc41a0b90c8b78a38597ce94268001a
 
 	/**
 	 * Launch the application.
@@ -42,21 +35,16 @@ public class V1 extends JFrame {
 	 */
 	public V1() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 483, 291);
+		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		{
-<<<<<<< HEAD
-			lblNewLabel = new JLabel("Nombre:");
-			lblNewLabel.setBounds(10, 24, 44, 12);
+			lblNewLabel = new JLabel("Sistema De Gestión Hospitalaria");
+			lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
+			lblNewLabel.setBounds(94, 11, 266, 22);
 			contentPane.add(lblNewLabel);
-=======
-			btnNewButton = new JButton("New button");
-			btnNewButton.setBounds(10, 11, 89, 23);
-			contentPane.add(btnNewButton);
->>>>>>> 2893f46f2cc41a0b90c8b78a38597ce94268001a
 		}
 
 	}
